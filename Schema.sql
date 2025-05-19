7b16184c560f8687888ef9cbae728fdcca2aa278
AmanBanothe <banotheaman@gmail.com>
2025-05-20 00:03:31 +0530

    Add files via upload

---
+++ Schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE online_retail (
+InvoiceNo VARCHAR(20),
+StockCode VARCHAR(20),
+Description TEXT,
+Quantity INT,
+InvoiceDate DATETIME,
+UnitPrice DECIMAL(10, 2),
+CustomerID INT,
+Country VARCHAR(50)
+);
